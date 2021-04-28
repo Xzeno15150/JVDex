2c9534284ba7a67748b5a0d0f4df751e7abe29da
maorillon <maorillon@05cc8a1e-d6dd-4db3-b2a9-684ba0b42a03>
2021-04-28 08:12:29 +0000

    ajout de vues et utilisation des user controle

---
+++ JVDex/JVDex/Application Graphique/Theorie.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Application_Graphique
+{
+    /// <summary>
+    /// Logique d'interaction pour Theorie.xaml
+    /// </summary>
+    public partial class Theorie : Window
+    {
+        public Theorie()
+        {
+            InitializeComponent();
+        }
+    }
+}
