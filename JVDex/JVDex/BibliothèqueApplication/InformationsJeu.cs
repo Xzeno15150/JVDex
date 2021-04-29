1319e1c31237618f2155e778fe434e39139776b4
vigaillard2 <vigaillard2@05cc8a1e-d6dd-4db3-b2a9-684ba0b42a03>
2021-04-29 07:53:50 +0000

    Ajout d'un nouveau projet (BibliothèqueApplication) et début de la programmation du métier (classes Jeu, InformationsJeu et Nommable) à compléter

---
+++ JVDex/JVDex/BibliothèqueApplication/InformationsJeu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BibliothèqueApplication
+{
+    public class InformationsJeu:Nommable
+    {
+        private HashSet<Genres> lesGenres;
+        private HashSet<Plateformes> lesPlateformes;
+        private string nomCreateur1;
+
+        enum Genres
+        {
+            Plateforme,
+            RPG,
+            FPS,
+            Infiltration,
+            Espionnage,
+            Reflexe,
+            Puzzle,
+            Survival_Horror,
+            Simulation,
+            Stratégie,
+            Sport
+        }
+        //TODO Verifier qu'il ne manque pas de genres
+        enum Plateformes
+        {
+            PC,
+            Playstation1,
+            Playstation2,
+            Playstation3,
+            Playstation4,
+            Playstation5
+        }
+        //TODO Faire l'enum des plateformes
+
+        //PROPRIÉTÉS
+        public string NomCreateur { get; set; }
+        public DateTime DateCreation { get; set; }
+        public int LimiteAge { get; set; }
+        public string Sysnopsis { get; set; }
+
+    }
+}
---
+++ JVDex/JVDex/BibliothèqueApplication/Jeu.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BibliothèqueApplication
+{
+    public class Jeu
+    {
+        private HashSet<string> lesVisuels;
+        private IList<string> lesMusiques;
+        private bool isFavoris;
+
+        public bool GetIsFavoris()
+        {
+            return isFavoris;
+        }
+
+        public void InverserIsFavoris()
+        {
+            isFavoris = !isFavoris;
+        }
+
+
+    }
+}
