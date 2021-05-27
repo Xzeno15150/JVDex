105230808ed938d7da1c152feaf3b24933af4c7b
maorillon <maorillon@05cc8a1e-d6dd-4db3-b2a9-684ba0b42a03>
2021-05-27 17:57:00 +0000

    modification de la doc

--- JVDex/JVDex/BibliothèqueApplication/Visuel.cs
+++ JVDex/JVDex/BibliothèqueApplication/Visuel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BibliothèqueApplication
 {
-    class Visuel
+    class Visuel : IEquatable<Visuel>
     {
         public string Vignette { get; set; }
         public string Legende { get; set; }
@@ -24,7 +24,22 @@ namespace BibliothèqueApplication
 
         public override bool Equals(object obj)
         {
-            return base.Equals(obj);
+
+            if (this == obj) return true;
+            if (obj is null) return false;
+            if (!GetType().Equals(obj.GetType())) return false;
+
+            Visuel other = obj as Visuel;
+
+            return Equals(other);
+            
+        }
+
+        public bool Equals(Visuel other)
+        {
+            if (other is null) return false;
+            return Vignette.Equals(other.Vignette) &&
+                   Legende.Equals(other.Legende);
         }
 
         public override int GetHashCode()
