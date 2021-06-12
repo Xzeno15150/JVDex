87ae75533ac8ec0a3ec59bc4eb30d85a37d96c80
maorillon <maorillon@05cc8a1e-d6dd-4db3-b2a9-684ba0b42a03>
2021-06-12 10:00:13 +0000

    la franchise mario faite

--- JVDex/JVDex/BibliothèqueApplication/Enums/Genres.cs
+++ JVDex/JVDex/BibliothèqueApplication/Enums/Genres.cs
@@ -5,20 +5,21 @@ namespace BibliothèqueApplication
 {
     public enum Genres
     {
-        Plateforme,
-        RPG,
-        FPS,
-        Action,
-        Aventure,
-        Infiltration,
-        Espionnage,
-        Reflexe,
-        Puzzle,
+        Plateforme, //0
+        RPG,//1
+        FPS,//2
+        Action,//3
+        Aventure,//4
+        Infiltration,//5
+        Espionnage,//6
+        Reflexe,//7
+        Puzzle,//8
         [EnumDescription("Survival Horror")]
-        Survival_Horror,
-        Simulation,
-        Stratégie,
-        Sport
+        Survival_Horror,//9
+        Simulation,//10
+        Stratégie,//11
+        Sport,//12
+        Course//13
     }
 }
 //enum des différents genres des jeux
